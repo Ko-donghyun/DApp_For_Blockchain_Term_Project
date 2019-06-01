259bdde7894c2207a6ba9d7d9d3025abc06169d5
kodonghyun <kodonghyun1209@gmail.com>
2019-06-01 13:24:15 +0900

    Request List 관리를 위한 코드 추가

--- PPDL_PK_RL_BL.sol
+++ PPDL_PK_RL_BL.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.1;
 contract PPDL_HELPER {
     // This creates an array with all balances
     mapping (address => string) public_key;
+    mapping (address => address[]) request_list;
 
     // # Public Key
     function deploy_public_key(string memory _value) public {
@@ -10,5 +11,13 @@ contract PPDL_HELPER {
     }
     function get_public_key(address _target) view public returns (string memory) {
         return public_key[_target];
+    }
+
+    // # Request List
+    function register_request_list(address[] memory _addresses) public {
+        request_list[msg.sender] = _addresses;
+    }
+    function get_request_list(address _target) view public returns (address[] memory) {
+        return request_list[_target];
     }
 }
