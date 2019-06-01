0f95f82f1e9bc2e680fc31c96e0a1a88f67da6c1
kodonghyun <kodonghyun1209@gmail.com>
2019-06-01 14:57:48 +0900

    Public Key 배포 컨트롤러 및 라우터 추가

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,7 @@
-const NodeRSA = require('node-rsa');/** Private Key 생성 */
+const NodeRSA = require('node-rsa');
+const fs = require('fs');
+
+/** Private Key 생성 */
 exports.generate_private_key = (req, res) => {
     console.log(`Private Key 생성 시작`);
 
@@ -9,4 +12,21 @@ exports.generate_private_key = (req, res) => {
     // 2. Private Key 리턴
     console.log(`Private Key 생성 완료`);
     return res.json({key: private_key});
+};
+
+/** Public Key 배포 */
+exports.deploy_public_key = (req, res) => {
+    console.log(`Public Key 배포 시작`);
+    const private_key_path = req.query.private_key_path;
+
+    fs.readFile(private_key_path, 'utf8', function(err, data) {
+        // 1. Public Key 생성
+        const key = new NodeRSA(data);
+        const public_key = key.exportKey(['public']);
+        console.log(public_key);
+
+        // 2. Public Key 배포
+        console.log(`Public Key 배포 완료`);
+        return res.json({success: 0, key: public_key});
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,7 @@ router.get('/', function(req, res, next) {
 
 // Private Key 생성
 router.get('/generate_key',  controller.generate_private_key);
+// Public Key 배포
+router.get('/deploy_key',  controller.deploy_public_key);
+
 module.exports = router;
